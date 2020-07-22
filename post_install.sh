11d6a2e29acfea0bc1caa24798cee48b9218742a
Vishnu <narayanannampoothiry@gmail.com>
2020-07-23 01:54:14 +0530

    First

---
+++ post_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Output info
+echo "Installing OpenSpeedTest..."
+echo "After installation, please mount your pool inside the jail to /mnt/fnpool"
+echo "A daily crontab has been installed to index the storage pool. If time period is not desirable, enter the jail and run "crontab -e" to modify."
+
+# Run bash script to install
+chmod +x /root/installdiskover.sh
+/usr/local/bin/bash /root/installdiskover.sh
+
+echo "Installing Diskover..." >> /root/PLUGIN_INFO
+echo "After installation, please mount your pool inside the jail to /mnt/fnpool" >> /root/PLUGIN_INFO
+echo "A daily crontab has been installed to index the storage pool. If time period is not desirable, enter the jail and run "crontab -e" to modify." >> /root/PLUGIN_INFO
+exit
