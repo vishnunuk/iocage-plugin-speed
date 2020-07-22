db2ab875162a80ff086b64ef14e8576424ecb826
Vishnu <narayanannampoothiry@gmail.com>
2020-07-23 03:15:39 +0530

    testdds2

--- post_install.sh
+++ post_install.sh
@@ -5,8 +5,8 @@ echo "After installation, please mount your pool inside the jail to /mnt/fnpool"
 echo "A daily crontab has been installed to index the storage pool. If time period is not desirable, enter the jail and run "crontab -e" to modify."
 
 # Run bash script to install
-chmod +x /root/installdiskover.sh
-/usr/local/bin/bash /root/installdiskover.sh
+chmod +x installopenspeedtest.sh
+/usr/local/bin/bash /root/installopenspeedtest.sh
 
 echo "Installing Diskover..." >> /root/PLUGIN_INFO
 echo "After installation, please mount your pool inside the jail to /mnt/fnpool" >> /root/PLUGIN_INFO
