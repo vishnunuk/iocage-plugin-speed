0c5076041db367cf576401a53e10df91692980de
Vishnu <narayanannampoothiry@gmail.com>
2020-07-23 06:18:54 +0530

    testddsk

--- overlay/root/installopenspeedtest.sh
+++ overlay/root/installopenspeedtest.sh
@@ -1,26 +1,4 @@
 #!/bin/sh
-########################################################################
-# Type: Installation file for shirosaidevÂ´s Diskover
-# ----------------------------------------------------------------------
-# Summary: Batch file to run inside a Jail @ FreeNAS or FreeBSD
-# ----------------------------------------------------------------------
-# Warning: I have NOT tighten the security of the jail
-# ----------------------------------------------------------------------
-# Description:
-#    This is installes all Elasticsearch 5.6 , Redis 4.x , Python 3.7
-#    with required modules , and PHP 7.2 with extenshions, and modules.
-#    It also creates a Nginx webserver for the diskover-web module. 
-#    You must run #pkg install bash and use bash to run the script.
-#    
-#    diskover is written and maintained by Chris Park (shirosai) 
-#    and runs on Linux and OS X/macOS using Python 2/3.
-#    
-#    This sh file for FreeBSD is made by Andreas M Aanerud ( Aanerud )
-# ----------------------------------------------------------------------
-# changelist:
-#    2019/02/18: 1st version
-########################################################################
-
 # Installing Nginx
     clear
     echo '--------------------------------------------------------------------------------'
--- post_install.sh
+++ post_install.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 # Output info
 echo "Installing OpenSpeedTest..."
-echo "After installation, please mount your pool inside the jail to /mnt/fnpool"
-echo "A daily crontab has been installed to index the storage pool. If time period is not desirable, enter the jail and run "crontab -e" to modify."
-
-# Run bash script to install
+ 
 chmod +x /root/installopenspeedtest.sh
 /bin/sh /root/installopenspeedtest.sh
-
-#/root/post_install.sh: /usr/local/bin/bash: not found
-
-echo "Installing Diskover..." >> /PLUGIN_INFO
-echo "After installation, please mount your pool inside the jail to /mnt/fnpool" >> /root/PLUGIN_INFO
-echo "A daily crontab has been installed to index the storage pool. If time period is not desirable, enter the jail and run "crontab -e" to modify." >> /root/PLUGIN_INFO
-exit
+ 
